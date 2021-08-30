321de409676664fafe875a60c720143a98b98ce8
geraldo.pires <geraldopireslacerda771@gmail.com>
2021-08-30 09:57:28 -0300

    Configurando a classe Repository

--- crudROOMCOMPOSE/app/src/main/java/com/example/crudroomcompose/data/repositories/CidadesRepository.kt
+++ crudROOMCOMPOSE/app/src/main/java/com/example/crudroomcompose/data/repositories/CidadesRepository.kt
@@ -1,4 +1,28 @@
 package com.example.crudroomcompose.data.repositories
 
-class CidadesRepository {
+import com.example.crudroomcompose.data.database.AppDB
+import com.example.crudroomcompose.data.entities.Cidades
+import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.flow.Flow
+
+import javax.inject.Inject
+
+
+@ViewModelScoped
+class CidadesRepository @Inject constructor(appDB: AppDB){
+
+    private val dao = appDB.CidadeDAO()
+
+    val getAllCidades: Flow<List<Cidades>> = dao.getAllCidades()
+
+    suspend fun addCidade(c: Cidades){
+        dao.insert(c)
+    }
+    suspend fun updateCidade(c: Cidades){
+        dao.update(c)
+    }
+    suspend fun deleteCidade(c: Cidades){
+        dao.delete(c)
+    }
+
 }
