ac7aa90a7977d3126dab9175405f0e8181092225
geraldo <geraldopireslacerda771@gmail.com>
2021-08-30 22:16:16 -0300

    Configurando Retrofit 2 com Hilt

--- ExemploComposeAPI/app/src/main/java/com/example/exemplocomposeapi/Utils/DigimonHilt.kt
+++ ExemploComposeAPI/app/src/main/java/com/example/exemplocomposeapi/Utils/DigimonHilt.kt
@@ -1,10 +1,15 @@
 package com.example.exemplocomposeapi.Utils
 
 import android.app.Application
+import com.example.exemplocomposeapi.api.DigimonApi
+import com.example.exemplocomposeapi.api.DigimonRepository
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.HiltAndroidApp
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @HiltAndroidApp
@@ -14,5 +19,19 @@ class DigimonHilt: Application() {
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule{
+
+    @Singleton
+    @Provides
+    fun provideDigimonResporitory(api: DigimonApi) = DigimonRepository(api)
+
+    @Singleton
+    @Provides
+    fun provideDigimonApi(): DigimonApi{
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://digimon-api.vercel.app/api/")
+            .build()
+            .create(DigimonApi::class.java)
+    }
 
 }
